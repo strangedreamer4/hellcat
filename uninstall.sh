19afcc6ea0fdc31ec4f1f955b84715b34768bfec
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-08-14 21:05:49 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,17 @@
-
 #!/bin/bash
 
-REPO_DIR="hellcat"
+# Define the repository name
+REPO_NAME="hellcat"
 
 # Function to uninstall the repository
 uninstall_repo() {
-    if [ -d "$REPO_DIR" ]; then
+    # Find the absolute path of the repository directory
+    REPO_DIR=$(find ~ -type d -name "$REPO_NAME" -print -quit 2>/dev/null)
+
+    if [ -n "$REPO_DIR" ]; then
         echo "Removing repository..."
         rm -rf "$REPO_DIR"
+        echo "Repository removed."
     else
         echo "Repository directory not found."
     fi
@@ -15,7 +19,6 @@ uninstall_repo() {
 
 # Run the uninstall_repo function
 uninstall_repo
-
 sleep 2
 pip uninstall -y tk
 pip uninstall -y Pillow
