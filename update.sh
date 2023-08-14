7710ad07acd82147ef98121d630a924b90ba0eb6
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-08-15 00:05:01 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+# Navigate to the parent directory
+cd ..
 
+# Remove the old repository directory
+if [ -d "$REPO_DIR" ]; then
+    echo "Removing previous repository..."
+    rm -rf "$REPO_DIR"
+fi
+
+# Clone the new repository
+echo "Cloning repository..."
+git clone "$REPO_URL" "$REPO_DIR"
+
+echo "Update complete!"
 # Define the repository URL
 REPO_URL="https://github.com/strangedreamer4/hellcat.git"
 
