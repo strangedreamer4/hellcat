c55a1a6573de586d26e790b50e1428cbc2246479
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-08-14 23:47:34 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,19 +2,17 @@
 
 # Define the repository URL
 REPO_URL="https://github.com/strangedreamer4/hellcat.git"
+REPO_DIR="hellcat"
 
-# Define a function to update the repository
+# Function to update the repository
 update_repo() {
-    # Check if the repository directory already exists
-    if [ -d "hellcat" ]; then
-        echo "Updating repository..."
-        cd hellcat
-        git pull origin main
-        cd ..
-    else
-        echo "Cloning repository..."
-        git clone $REPO_URL hellcat
+    if [ -d "$REPO_DIR" ]; then
+        echo "Removing previous repository..."
+        rm -rf "$REPO_DIR"
     fi
+
+    echo "Cloning repository..."
+    git clone "$REPO_URL" "$REPO_DIR"
 }
 
 # Run the update_repo function
