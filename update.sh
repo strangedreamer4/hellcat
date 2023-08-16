336fd3b9552e8e0b767cba5a71e3b9694093b1a3
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-08-16 23:30:48 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,25 +13,42 @@ echo "Cloning repository..."
 git clone "$REPO_URL" "$REPO_DIR"
 
 echo "Update complete!"
+
+#!/bin/bash
+
 # Define the repository URL
 REPO_URL="https://github.com/strangedreamer4/hellcat.git"
+# Define the repository directory
+REPO_DIR="hellcat"
+
+# Navigate to the parent directory
+cd ..
 
 # Define a function to update the repository
 update_repo() {
     # Check if the repository directory already exists
-    if [ -d "hellcat" ]; then
-        if [ -d "hellcat/.git" ]; then
+    if [ -d "$REPO_DIR" ]; then
+        if [ -d "$REPO_DIR/.git" ]; then
             echo "Updating repository..."
-            cd hellcat
+            cd "$REPO_DIR"
+            
+            # Stash local changes if any
+            git stash
+            
+            # Pull changes from the remote repository
             git pull origin main
+            
+            # Apply stashed changes if any
+            git stash pop
+            
             cd ..
         else
-            echo "The 'hellcat' directory exists but is not a Git repository."
+            echo "The '$REPO_DIR' directory exists but is not a Git repository."
             echo "Please manually remove or backup the directory and re-run the script."
         fi
     else
         echo "Cloning repository..."
-        git clone "$REPO_URL" hellcat
+        git clone "$REPO_URL" "$REPO_DIR"
     fi
 }
 
